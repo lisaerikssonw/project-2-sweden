26777bc4f9e00253199b3fd0ab8f314e72a753d4
Anosh D. Ullenius <anosh@anosh.se>
2019-05-06 20:30:35 +0200

    Debug code for testing environment variables. And updating package-lock after running npm install

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+require('dotenv').config();
 
 function App() {
   return (
@@ -6,5 +7,11 @@ function App() {
     </div>
   );
 }
+
+// debug environment variables
+let romeKey = process.env.REACT_APP_ROME_SECRET_KEY;
+let googleKey = process.env.REACT_APP_GOOGLE_SECRET_KEY;
+console.log("rome 2 rio key = " + romeKey);
+console.log("google key = " + googleKey);
 
 export default App;
