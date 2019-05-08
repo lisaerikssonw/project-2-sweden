b18d0b56fcff789514c16d3dc3d394d3066c5b8f
Anosh D. Ullenius <anosh@anosh.se>
2019-05-08 08:39:58 +0200

    Minor. Using constants for storing API-keys

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ class App extends Component {
 }
 
 // debug environment variables
-let romeKey = process.env.REACT_APP_ROME_SECRET_KEY;
-let googleKey = process.env.REACT_APP_GOOGLE_SECRET_KEY;
+const romeKey = process.env.REACT_APP_ROME_SECRET_KEY;
+const googleKey = process.env.REACT_APP_GOOGLE_SECRET_KEY;
 console.log("rome 2 rio key = " + romeKey);
 console.log("google key = " + googleKey);
 
