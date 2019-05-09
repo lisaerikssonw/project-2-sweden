c1ee9b0b27bbac3f0295b7c49b1135f1a9023ea6
Lisa <lisa.eriksson.w@gmail.com>
2019-05-09 14:34:33 +0200

    Style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render(){
         return (
-            <footer>
-                <h1>Ett Event,en möjlighet!</h1>
+            <footer className="footer">
+                <div className="footer-heading"><h1>ETT EVENT - EN MÖJLIGHET!</h1></div>
                 <p>Vi leder alla till Sverige. To Sweden i samarbete med Svenska Olympiska kommittén och den internationella olympiska kommittén.</p>
             </footer>
         )
