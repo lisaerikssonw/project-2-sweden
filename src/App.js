ff2cf6ad6120b980a04bd4721af0c52a868c0aed
Anosh D. Ullenius <anosh@anosh.se>
2019-05-16 22:45:18 +0200

    Adding car icon. Removing Home from nav-bar menu

--- src/App.js
+++ src/App.js
@@ -114,8 +114,7 @@ class App extends Component {
               {/* menu block goes here*/}
 
               <div className="nav-container">
-                <button className="button hidden" onClick={()=> this.setState({page:"home"})}><img className="icon" src={process.env.PUBLIC_URL + "/images/icons/rings.png"} alt="Olympic rings" title="Home" />Home</button>
-                <button className="button">Search Trips</button>
+                <button className="button" onClick={()=> this.setState({page:"home"})}><img className="icon" src={process.env.PUBLIC_URL + "/images/icons/rings.png"} alt="Olympic rings" title="Home" />Search Trips</button>
                 <button className="button">About the Event</button>
                 <button className="dropdown">About our<br />Destinations
                   <div className="dropdown-content">
