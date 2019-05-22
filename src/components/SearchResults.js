14be09125b8e26dae69beae6faccab4bc92d33a8
Lisa <lisa.eriksson.w@gmail.com>
2019-05-22 20:55:55 +0200

    Added arrow icon for sorting

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -49,6 +49,12 @@ class SearchResults extends Component {
 
   render() {
 
+
+      const sortIcon = <img className="sort-arrow"
+      src={process.env.PUBLIC_URL + "/images/icons/sort-arrow.png"}
+      alt="sort-arrow"
+      title="Sort" />
+
       const sortFunction = this.setSort()
       const routeList = this.props.routes
       .sort(sortFunction)
@@ -66,12 +72,10 @@ class SearchResults extends Component {
                   <th>From</th>
                   <th>To</th>
                   <th>Means of Travel</th>
-                  <th onClick={() => this.setColumnState('time')} >Time</th>
-                  <th onClick={() => this.setColumnState('price')} >Price</th>
-                  <th onClick={() => this.setColumnState('distance')}
-                      className="hidden">Distance</th>
-                  <th onClick={() => this.setColumnState('transits')}
-                      className="hidden">No. Transits</th>
+                  <th onClick={() => this.setColumnState('time')} >Time {sortIcon} </th>
+                  <th onClick={() => this.setColumnState('price')} >Price {sortIcon} </th>
+                  <th onClick={() => this.setColumnState('distance')} className="hidden">Distance {sortIcon} </th>
+                  <th onClick={() => this.setColumnState('transits')} className="hidden">No. Transits {sortIcon} </th>
                 </tr>
                 {routeList}
                 </tbody>
