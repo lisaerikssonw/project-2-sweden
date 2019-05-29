0e01ffd5eab78b9b407a645361e58573274b5fe2
Lisa <lisa.eriksson.w@gmail.com>
2019-05-29 14:31:33 +0200

    City articles in progress

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -3,11 +3,19 @@ import '../styles/mainbody.css'
 import SearchResults from './SearchResults';
 import SearchForm from './SearchForm';
 import FalunText from '../data-texts/falun-info-text.json';
+import StockholmText from '../data-texts/stockholm-info-text.json'
+import AreText from '../data-texts/are-info-text.json'
 import React, { Component } from 'react';
 
 
 const falunJson = JSON.stringify(FalunText);
 const falunInfo = JSON.parse(falunJson);
+
+const stockholmJson = JSON.stringify(StockholmText);
+const stockholmInfo = JSON.parse(stockholmJson);
+
+const areJson = JSON.stringify(AreText);
+const areInfo = JSON.parse(areJson);
 
 class MainBody extends Component {
     render() {
@@ -40,27 +48,45 @@ class MainBody extends Component {
             )
         }else if(this.props.page==="falun"){
             return (
-                <article>
-                <h1 className="Header-text">FALUN</h1>
-                <p className="info-text">{falunInfo.text}</p>
+                <article className="info-article">
+                    <img src={process.env.PUBLIC_URL + "/images/falun.png"}
+                        className="city-img"
+                        alt="Image of Falun"
+                        title="falun" />
+                    <div className="info-box">
+                        <h1 className="header-text">FALUN</h1>
+                        <p className="info-text">{falunInfo.text[0]}<br/><br/>{falunInfo.text[1]}</p>
+                    </div>
                 </article>
             )
 
         }else if(this.props.page==="are"){
             return (
-                <article>
-                  <h1>Åre</h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-                  </p>
+                <article className="info-article">
+                    <img src={process.env.PUBLIC_URL + "/images/aurora.jpg"}
+                        className="city-img"
+                        alt="Image of Åre"
+                        title="are" />
+                    <div className="info-box">
+                        <h1 className="header-text">Åre</h1>
+                        <p className="info-text">{areInfo.text[0]}<br/><br/>{areInfo.text[1]}
+                        </p>
+                    </div>
                 </article>
             )
 
         }else if(this.props.page==="stockholm"){
             return (
-                <article>
-                  <h1>Stockholm</h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-                  </p>
+                <article className="info-article">
+                    <img src={process.env.PUBLIC_URL + "/images/stockholm.png"}
+                        className="city-img"
+                        alt="Image of Stockholm"
+                        title="stockholm" />
+                    <div className="info-box">
+                        <h1 className="header-text">Stockholm</h1>
+                        <p className="info-text">{stockholmInfo.text[0]}<br/><br/>{stockholmInfo.text[1]}<br/><br/>{stockholmInfo.text[2]}
+                        </p>
+                  </div>
                 </article>
             )
 
