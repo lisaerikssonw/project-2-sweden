903c627372d42d9dd6ecf79d4a2703846ae90c33
PatrikFreij <pat.freij@gmail.com>
2019-06-05 13:04:19 +0200

    Fixed bug for Chrome not allowing uppercase for navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,18 +8,18 @@ const Nav = (props) => (
                     src={process.env.PUBLIC_URL + "/images/icons/rings.png"}
                     alt="Olympic rings"
                     title="Winter Olympics 2024" />
-                <button className="button-style"
+                <button className="button-style nav-uppercase"
                     onClick={()=> props.handlePageState("home")}>Search Trips
                 </button>
-                <button className="button-style">About the Event</button>
-                <button className="button-style dropdown">About our<br />Destinations
+                <button className="button-style nav-uppercase">About the Event</button>
+                <button className="button-style dropdown nav-uppercase">About our<br />Destinations
                   <div className="dropdown-content">
                     <div onClick={() => props.handlePageState("falun")}>Falun</div>
                     <div onClick={() => props.handlePageState("stockholm")}>Stockholm</div>
                     <div onClick={() => props.handlePageState("are")}>Åre</div>
                   </div>
                 </button>
-                <button className="button-style hidden">View Recommended</button>
+                <button className="button-style hidden nav-uppercase">View Recommended</button>
               </div>
             </nav>
 )
